b3b8ff9d1cb6888e4968fdb813dec15aa6c5b39f
eliyas41 <fentaeliyas@gmail.com>
2024-05-18 11:11:00 +0300

    Set up header component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import style from "./banner.module.css"
-import Header from '../Header/Header'
 
 const Banner = () => {
   return (
-    <section className={style.bg__img}>
-      <Header />
+    <section className={`h-50p md:h-full ${style.bg__img}`}>
+      <header className={`flex flex-row justify-between md:justify-end ${style.header__wrapper}`}>
+        <div className='text-white md:hidden'>Logo</div>
+        <div className='text-white flex flex-row w-1/2 justify-between'>
+          <div>Time</div>
+          <div>Weather</div>
+          <div>SearchIcon</div>
+          <div className='hidden md:block'>User</div>
+        </div>
+      </header>
     </section>
   )
 }

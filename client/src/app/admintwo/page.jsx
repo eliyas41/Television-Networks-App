d174674b7e433e9ce6fb369d26b0b0806b404804
eliyas41 <fentaeliyas@gmail.com>
2024-05-21 02:27:52 +0300

    Restructured folder and Complete Admin Panel UI

---
+++ client/src/app/admintwo/page.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+
+const page = ({ children }) => {
+  return (
+    <div >
+      {children}
+    </div>
+  )
+}
+
+export default page;
+
+
+
+
+// https://www.youtube.com/watch?v=N51ik3kUqxE
---
+++ client/src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '@/components/Home';
+
+const page = () => {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default page;

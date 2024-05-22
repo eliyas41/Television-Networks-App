7c777af5572b59e15b387f3b25e53cce46a97e43
eliyas41 <fentaeliyas@gmail.com>
2024-05-22 23:30:08 +0300

    Hundle search filtering Functionality

--- client/src/app/movies/page.jsx
+++ client/src/app/movies/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Card from '@mui/material/Card';
@@ -14,92 +14,79 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import CloudRoundedIcon from '@mui/icons-material/CloudRounded';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import NavBar from "../../components/Nav/NavBar"
-import style from "./page.module.css"
-import axios from "../../axios.config"
+import NavBar from "../../components/Nav/NavBar";
+import style from "./page.module.css";
+import axios from "../../axios.config";
 
 export default function MenuAppBar() {
   const [movies, setMovies] = useState([]);
-  console.log(movies)
+  const [search, setSearch] = useState('');
+  const [network, setNetwork] = useState('');
+  const [category, setCategory] = useState('');
+  const [page, setPage] = useState(1);
 
   const getMovies = async () => {
     try {
-      const response = await axios.get("/movies");
-      console.log(response.data.movies)
+      const response = await axios.get('/movies', {
+        params: {
+          search,
+          networkId: network,
+          categoryId: category,
+          page,
+          limit: 10,
+        },
+      });
       setMovies(response.data.movies);
     } catch (error) {
-      console.log(error)
+      console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
     getMovies();
-  }, [])
+  }, [search, network, category, page]);
 
   function formatDuration(durationInMilliseconds) {
-    // Convert milliseconds to seconds
     const durationInSeconds = durationInMilliseconds / 1000;
-
-    // Calculate hours, minutes, and seconds
     const hours = Math.floor(durationInSeconds / 3600);
     const minutes = Math.floor((durationInSeconds % 3600) / 60);
-    // const seconds = Math.floor(durationInSeconds % 60);
-
-    // Pad with zeros and format
     const formattedHours = String(hours).padStart(2, '0');
     const formattedMinutes = String(minutes).padStart(2, '0');
-    // const formattedSeconds = String(seconds).padStart(2, '0');
-
-    // Return formatted duration
     return `${formattedHours}:${formattedMinutes}`;
   }
-
-
 
   return (
     <section className='flex'>
       <div className='bg-custom-white-light hidden md:block md:w-[12%]'>
         <NavBar />
       </div>
-
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" sx={{ bgcolor: '#1B1C3A', height: "150px", paddingTop: "40px" }}>
           <Toolbar>
             <Link href="/">
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                color="inherit"
-              >
+              <IconButton size="large" color="inherit">
                 <ArrowBackIosIcon className='rounded-full bg-custom-white-light' />
               </IconButton>
             </Link>
-
             <Typography variant="h6" component="div" sx={{ flexGrow: 1, paddingLeft: "10px" }}>
               Movies
             </Typography>
-
-            <header className="w-[30%]">
+            <header className="w-[40%]">
               <div className='text-white flex justify-between mr-14'>
                 <div className='hidden md:block'><CloudRoundedIcon /> <small><span>18<sup>o</sup></span></small></div>
                 <div className='hidden md:block'><small>5:30PM</small></div>
-                <div className='text-gray-300 pl-14'><SearchRoundedIcon /></div>
+                <input
+                  type="text"
+                  placeholder="Search movies..."
+                  className='text-gray-600 pl-1 h-6 w-28 md:w-48'
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
               </div>
             </header>
-
-
             <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                color="inherit"
-              >
+              <IconButton size="large" color="inherit">
                 <AccountCircle />
               </IconButton>
             </div>
@@ -113,49 +100,43 @@ export default function MenuAppBar() {
           </div>
         </AppBar>
 
-
         <div className={`${style.poster__container} bg-slate-800 flex flex-col md:flex-row gap-3 justify-between items-center`}>
-          {
-            movies.map((movie, id) => {
-              const videoLink = movie.videoUrl;
-              const duration = movie.duration;
-              const title = movie.title;
-              return (
-                <Link href={videoLink} target='blank'>
-                  <Card sx={{ maxWidth: 300, height: 400, marginTop: 8 }} className={`${style.card__container} flex flex-cow md:flex-col`}>
-                    <div className='pt-72 md:pt-0'>
-                      <CardContent>
-                        <Typography variant="body2" color="text.primary">
-                          <small className='text-xl text-white font-bold'>{title}</small>
-                        </Typography>
-                      </CardContent>
+          {movies.filter((movie) => {
+            return search.toLowerCase() === '' ? movie : movie.title.toLowerCase().includes(search);
+          }).map((movie, id) => {
+            const videoLink = movie.videoUrl;
+            const duration = movie.duration;
+            const title = movie.title;
+            return (
+              <Link key={id} href={videoLink} target='blank'>
+                <Card sx={{ maxWidth: 300, height: 400, marginTop: 8 }} className={`${style.card__container} flex flex-cow md:flex-col`}>
+                  <div className='pt-72 md:pt-0'>
+                    <CardContent>
+                      <Typography variant="body2" color="text.primary">
+                        <small className='text-xl text-white font-bold'>{title}</small>
+                      </Typography>
+                    </CardContent>
+                  </div>
+                  <CardActions className='flex flex-col' disableSpacing>
+                    <div className='text-white font-medium md:pl-48'>
+                      {formatDuration(duration)}
                     </div>
-
-                    <CardActions className='flex flex-col' disableSpacing>
-                      <div className='text-white font-medium md:pl-48'>
-                        {formatDuration(duration)}
-                      </div>
-
-                      <div className='flex flex-col md:flex-row pt-28 md:pt-60 gap-8 md:gap-3'>
-                        <IconButton aria-label="add to favorites">
-                          <PlayCircleOutlineRoundedIcon sx={{ color: "white", fontSize: "27px" }} />
-                        </IconButton>
-
-                        <IconButton aria-label="add to favorites">
-                          <AccessTimeRoundedIcon sx={{ color: "white", fontSize: "27px" }} />
-                        </IconButton>
-
-                        <IconButton aria-label="add to favorites">
-                          <FavoriteBorderRoundedIcon sx={{ color: "white", fontSize: "27px" }} />
-                        </IconButton>
-                      </div>
-
-                    </CardActions>
-                  </Card>
-                </Link>
-              )
-            })
-          }
+                    <div className='flex flex-col md:flex-row pt-28 md:pt-60 gap-8 md:gap-3'>
+                      <IconButton aria-label="add to favorites">
+                        <PlayCircleOutlineRoundedIcon sx={{ color: "white", fontSize: "27px" }} />
+                      </IconButton>
+                      <IconButton aria-label="add to favorites">
+                        <AccessTimeRoundedIcon sx={{ color: "white", fontSize: "27px" }} />
+                      </IconButton>
+                      <IconButton aria-label="add to favorites">
+                        <FavoriteBorderRoundedIcon sx={{ color: "white", fontSize: "27px" }} />
+                      </IconButton>
+                    </div>
+                  </CardActions>
+                </Card>
+              </Link>
+            );
+          })}
         </div>
       </Box>
     </section>
--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
 import style from "./banner.module.css"
 import Image from 'next/image'
 import Logo from "./logo1.jpeg"
@@ -7,7 +8,17 @@ import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import MovieCard from '../MovieCard/MovieCard';
 
-const Banner = () => {
+const Banner = ({ onSearch }) => {
+
+  const [query, setQuery] = useState('');
+
+  const handleChange = (e) => {
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    onSearch(newQuery);
+  };
+
+
   return (
     <>
       <section className={`h-50p md:h-full ${style.bg__img} ${style.hero__container}`}>
@@ -23,7 +34,14 @@ const Banner = () => {
           <div className='text-white flex flex-row w-1/2 justify-between'>
             <div> <small>5:30PM</small></div>
             <div><CloudRoundedIcon /> <small><span>18<sup>o</sup></span></small></div>
-            <div className='text-gray-300'><SearchRoundedIcon /></div>
+            <input
+              type="text"
+              placeholder="Search movies..."
+              className='text-gray-600 pl-1'
+              value={query}
+              onChange={handleChange}
+            /> <span> <div className='text-gray-300'><SearchRoundedIcon /></div></span>
+
             <div className='hidden md:block'><AccountCircleOutlinedIcon /></div>
           </div>
         </header>

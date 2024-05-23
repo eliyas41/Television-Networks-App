4886f5ed34b627a5ab8c78e16c4d9833a2d31a6a
eliyas41 <fentaeliyas@gmail.com>
2024-05-23 23:06:25 +0300

    Change local host API

--- client/src/axios.config.js
+++ client/src/axios.config.js
@@ -2,7 +2,10 @@ import axios from "axios"
 
 const axiosBaseUrl = axios.create({
   //Localhost
-  baseURL: 'http://localhost:8080/api',
+  // baseURL: 'http://localhost:8080/api',
+
+  //deployed version of television-networks-app server on render.com
+  baseURL: 'https://television-networks-app-gzf2.onrender.com/api',
 })
 
 export default axiosBaseUrl
